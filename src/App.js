85b4682d26b4d3157b7ff66e6d47090c6b817cbb
stella0905 <o122233@naver.com>
2023-03-16 16:29:35 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 
 import './App.css';
-import Letter from './Letter';
+import LetterEditor from './pages/LetterEditor';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import CheeringMessage from './pages/CheeringMessage';
+
+
+//COMPONENTS
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className='App.-header'>
-        <Letter />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={'왼쪽버튼'} onClick={() => alert("왼쪽클릭")} />}
+          rightChild={
+            <MyButton text={'오른쪽버튼'} onClick={() => alert("오른쪽클릭")} />}
+        />
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"negative"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"default"}
+        />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* 경로가 /일때 element는 Home이다 라는 의미 */}
+          <Route path='/LetterEditor' element={<LetterEditor />} />
+          <Route path='/CheeringMessage' element={<CheeringMessage />} />
+
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
