6d6103f2c156d4dff7d07e8e1a1a56736b786c55
stella0905 <o122233@naver.com>
2023-03-17 11:16:41 +0900

    FEAT COMMIT

---
+++ src/pages/CheeringMessageList.js
@@ -0,0 +1,11 @@
+const CheeringMessageList = () => {
+  return (
+    <></>
+  CheeringMessageList.defaultProps = {
+    CheeringMessageList: []
+    //App.js에서 diaryList를 받아올때 혹시나 undefined로 전달될 경우 에러가 뜨는걸 대비해서 빈배열을 넣어준다. 
+  }
+  )
+}
+
+export default CheeringMessageList
