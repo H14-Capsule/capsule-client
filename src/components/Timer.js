1efa7cbbf625305139d170f51e6430825aa1322e
stella0905 <o122233@naver.com>
2023-03-20 13:25:14 +0900

    feat: timer 추가

---
+++ src/components/Timer.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import Certification from '../pages/Certification';
+
+const Timer = ({ initialTime }) => {
+  const [timeLeft, setTimeLeft] = useState(initialTime);
+  const [isTimeOver, setIsTimeOver] = useState(false);
+
+
+  const tick = () => {
+    setTimeLeft((timeLeft) => {
+      if (timeLeft - 1 <= 0) {
+        setIsTimeOver(true);
+        return 0;
+      } else {
+        return timeLeft - 1;
+      }
+    });
+  };
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      tick();
+    }, 1000);
+    return () => clearInterval(timerId);
+  }, []);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
+
+
+  return (
+    <div>
+      <p>{`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}</p>
+    </div>
+  );
+};
+
+export default Timer;
